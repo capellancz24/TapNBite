358da84380f8a51b0a25b9d1a18e218f33f72c1b
capellancz24 <user.yunacplln@gmail.com>
2025-01-06 22:33:51 +0800

    Changed full name into first name & last name

---
+++ app/src/main/java/com/example/tapnbite_original/Checkout2_Activity.java
@@ -0,0 +1,47 @@
+package com.example.tapnbite_original;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.tapnbite_original.Fragments.Cart_Fragment;
+
+public class Checkout2_Activity extends AppCompatActivity {
+
+    private Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_checkout2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(Checkout2_Activity.this, R.color.primary));
+        getWindow().setNavigationBarColor(ContextCompat.getColor(Checkout2_Activity.this, R.color.white));
+
+        backButton = findViewById(R.id.backBtn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Cart_Fragment();
+
+                FragmentTransaction fn = getSupportFragmentManager().beginTransaction();
+                fn.replace(R.id.main, fragment).commit();
+            }
+        });
+    }
+}
