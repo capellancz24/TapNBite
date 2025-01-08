501106db8ca67942bcfedf49e15a0c3b60809e6e
capellancz24 <user.yunacplln@gmail.com>
2025-01-08 11:03:06 +0800

    Modified "On Boarding Activities"

--- app/src/main/java/com/example/tapnbite_original/Onboarding1_Activity.java
+++ app/src/main/java/com/example/tapnbite_original/Onboarding1_Activity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Onboarding_Activity extends AppCompatActivity {
+public class Onboarding1_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding);
+        setContentView(R.layout.activity_onboarding1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
